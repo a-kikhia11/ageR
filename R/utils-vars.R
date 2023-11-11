fee6efa5c8ec7f0ae97afb57877bd5ca28f3f15f
a-kikhia11 <145614245+a-kikhia11@users.noreply.github.com>
2023-11-11 23:37:45 +0200

    added vars to package utils

---
+++ R/utils-vars.R
@@ -0,0 +1,11 @@
+#' Select variables
+#'
+#' See \code{dplyr::\link[dplyer:select]{select}} for details.
+#'
+#' @name vars
+#' @rdname select
+#' @keywords internal
+#' @export
+#' @importFrom dplyr select
+#' @usage vars (...)
+NULL
