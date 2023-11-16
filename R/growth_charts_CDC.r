2f5ab55e9fcd832d21ee276bf397aefd153a146b
a-kikhia11 <145614245+a-kikhia11@users.noreply.github.com>
2023-11-16 13:50:09 +0200

    Updated Documentation

--- R/growth_charts_CDC.r
+++ R/growth_charts_CDC.r
@@ -1,24 +1,24 @@
-#' Data from growth charts
+#' Data From CDC (US) Growth Charts
 #'
 #' @details
 #' A data frame containing information for growth charts from the National Center from Health and Statistics. Visit \url{https://www.cdc.gov/growthcharts/percentile_data_files.htm} for more details.
 #'
 #' @format Data frame with 14 variables and 314 observations:
 #' \describe{
-#'    \item{Gender}{A character string. Refers to Male or Female.}
-#'    \item{Age}{Age in years and months}
-#'    \item{L (Power)}{Power in the Box-Cox transformation}
-#'    \item{M (Median)}{Median}
-#'    \item{S (CV)}{Generalized Coeficient of Variation}
-#'    \item{P3}{Data under percentile 3}
-#'    \item{P5}{Data under percentile 5}
-#'    \item{P10}{Data under percentile 10}
-#'    \item{P25}{Data under percentile 25}
-#'    \item{P50}{Data under percentile 50}
-#'    \item{P75}{Data under percentile 75}
-#'    \item{P90}{Data under percentile 90}
-#'    \item{P95}{Data under percentile 95}
-#'    \item{P97}{Data under percentile 97}
+#'    \item{\bold{Gender}}{A character string. Refers to Male or Female.}
+#'    \item{\bold{Age}}{Age in years and months}
+#'    \item{\bold{L (Power)}}{Power in the Box-Cox transformation}
+#'    \item{\bold{M (Median)}}{Median}
+#'    \item{\bold{S (CV)}}{Generalized Coeficient of Variation}
+#'    \item{\bold{P3}}{Data under percentile 3}
+#'    \item{\bold{P5}}{Data under percentile 5}
+#'    \item{\bold{P10}}{Data under percentile 10}
+#'    \item{\bold{P25}}{Data under percentile 25}
+#'    \item{\bold{P50}}{Data under percentile 50}
+#'    \item{\bold{P75}}{Data under percentile 75}
+#'    \item{\bold{P90}}{Data under percentile 90}
+#'    \item{\bold{P95}}{Data under percentile 95}
+#'    \item{\bold{P97}}{Data under percentile 97}
 #'
 #' }
 #' @usage CDC_curves
